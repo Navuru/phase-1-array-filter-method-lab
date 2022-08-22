5a7db94047bcf44b9f8528443aef3d82790acc57
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-08-22 11:15:23 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching (drivers,name) {
+    return drivers.filter(function (driver) {
+        return driver.toUpperCase().includes(name.toUpperCase());
+    })
+}
+
+function fuzzyMatch (drivers,name) {
+    return drivers.filter(function (driver){
+        return driver.toUpperCase().startsWith(name.toUpperCase());
+    })
+
+}
+function matchName (drivers,name) {
+    return drivers.filter(function (driver){
+        return driver.name === name;
+    })
+}
